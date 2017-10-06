94ac73e300b377d45eced6becba7b449701a53c7
iLuvmakonnen <32548337+iLuvmakonnen@users.noreply.github.com>
2017-10-06 19:40:21 -0400

    Permission fixed

---
+++ src/services/mock-categories.ts
@@ -0,0 +1,37 @@
+export let CATEGORIES = [
+  {
+    id: 1,
+    name: "Asian Food",
+    image: "http://img.huffingtonpost.com/asset/scalefit_970_noupscale/585be1aa1600002400bdf2a6.jpeg"
+    
+  },
+
+  {
+    id: 2,
+    name: "American Food",
+    image: "https://img.grouponcdn.com/deal/k3EyCBD149fq3nSARny/ft-2048x1229/v1/c700x420.jpg"
+    
+  },
+
+  {
+    id: 3,
+    name: "Mexican Food",
+    image: "http://wooderice.com/wp-content/uploads/2014/09/mexicanfood.jpg"
+    
+  },
+
+  {
+    id: 4,
+    name: "Colombian Food",
+    image: "https://images.latintravelguide.com/w700/colombian-food-and-drink-colombia-food-drink-61.jpg"
+    
+  },
+
+  {
+    id: 5,
+    name: "Jamaican Food",
+    image: "https://img.grouponcdn.com/iam/4joKLGeb6PmWXWquFzHt/3o-2048x1229/v1/c700x420.jpg"
+    
+  },
+  
+]
---
+++ src/services/mock-restaurants.ts
@@ -0,0 +1,204 @@
+export let RESTAURANTS = [
+  {
+    id: 1,
+    name: "Benares",
+    address: "Benares, 45 Murray Street, New York, NY 10007",
+    phone: "212-766-4900",
+    location: {
+      lat: 40.7141495,
+      lon: 74.0113972,
+      distance: 1.4
+    },
+    rating: 3.9,
+    scores: [
+      {
+        id: 1,
+        name: "Foodies",
+        score: 98
+      },
+      {
+        id: 2,
+        name: "Family",
+        score: 80
+      },
+      {
+        id: 3,
+        name: "Local",
+        score: 71
+      },
+      {
+        id: 4,
+        name: "Budget",
+        score: 64
+      },
+      {
+        id: 5,
+        name: "Adventure",
+        score: 59
+      }
+    ],
+    thumb: "assets/img/restaurant/thumb/img_1.jpg",
+    reviews: [
+      {
+        id: 1,
+        username: "Batman",
+        avatar: "http://hendersonandjones.com/wp-content/uploads/2014/04/placeholder_user.png",
+        from: "New York",
+        content: "Very Nice Place!",
+        rating: 4,
+        recommended_for: [1, 3]
+      },
+      {
+        id: 2,
+        username: "Batman",
+        avatar: "http://hendersonandjones.com/wp-content/uploads/2014/04/placeholder_user.png",
+        from: "New York",
+        content: "Very Nice Place!",
+        rating: 5,
+        recommended_for: [1, 2]
+      },
+      {
+        id: 3,
+        username: "Batman",
+        avatar: "http://hendersonandjones.com/wp-content/uploads/2014/04/placeholder_user.png",
+        from: "New York",
+        content: "Very Nice Place!",
+        rating: 5,
+        recommended_for: [2, 3]
+      },
+      {
+        id: 4,
+        username: "Batman",
+        avatar: "http://hendersonandjones.com/wp-content/uploads/2014/04/placeholder_user.png",
+        from: "New York",
+        content: "Very Nice Place!",
+        rating: 4,
+        recommended_for: [4, 3]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Jane",
+    address: "100 W Houston St, New York, NY 10012",
+    phone: "212-254-7000",
+    location: {
+      lat: 40.7273681,
+      lon: -74.002435,
+      distance: 0.5
+    },
+    rating: 4.0,
+    scores: [
+      {
+        id: 1,
+        name: "Budget",
+        score: 98
+      },
+      {
+        id: 2,
+        name: "Family",
+        score: 80
+      },
+      {
+        id: 3,
+        name: "Local",
+        score: 71
+      },
+      {
+        id: 4,
+        name: "Foodies",
+        score: 64
+      },
+      {
+        id: 5,
+        name: "Adventure",
+        score: 59
+      }
+    ],
+    thumb: "assets/img/restaurant/thumb/img_2.jpg",
+    reviews: []
+  },
+  {
+    id: 3,
+    name: "Jane",
+    address: "100 W Houston St, New York, NY 10012",
+    phone: "212-254-7000",
+    location: {
+      lat: 40.7273681,
+      lon: -74.002435,
+      distance: 0.5
+    },
+    rating: 4.0,
+    scores: [
+      {
+        id: 1,
+        name: "Budget",
+        score: 98
+      },
+      {
+        id: 2,
+        name: "Family",
+        score: 80
+      },
+      {
+        id: 3,
+        name: "Local",
+        score: 71
+      },
+      {
+        id: 4,
+        name: "Foodies",
+        score: 64
+      },
+      {
+        id: 5,
+        name: "Adventure",
+        score: 59
+      }
+    ],
+    thumb: "assets/img/restaurant/thumb/img_2.jpg",
+    reviews: []
+  },
+  {
+    id: 4,
+    name: "Jane",
+    address: "100 W Houston St, New York, NY 10012",
+    phone: "212-254-7000",
+    location: {
+      lat: 40.7273681,
+      lon: -74.002435,
+      distance: 0.5
+    },
+    rating: 4.0,
+    scores: [
+      {
+        id: 1,
+        name: "Budget",
+        score: 98
+      },
+      {
+        id: 2,
+        name: "Family",
+        score: 80
+      },
+      {
+        id: 3,
+        name: "Local",
+        score: 71
+      },
+      {
+        id: 4,
+        name: "Foodies",
+        score: 64
+      },
+      {
+        id: 5,
+        name: "Adventure",
+        score: 59
+      }
+    ],
+    thumb: "assets/img/restaurant/thumb/img_2.jpg",
+    reviews: []
+  }
+  
+]
